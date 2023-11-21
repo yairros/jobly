7c36ffa8bef7e7f3dfefe37b6b3071cbb5f04672
Yair Rosenschein <yairros@gmail.com>
2023-11-21 14:07:33 +0200

    5

--- server.js
+++ server.js
@@ -52,10 +52,6 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
-})
-
-app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
